b530422113f053729db06570df0b31c1141dc18b
zoropersonal <156231438+zoropersonal@users.noreply.github.com>
2024-02-24 20:40:24 +0530

    Update made on 24/02/24

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const fillIt = () => {
     console.log(sessionStorage.getItem("pwdChecked"));
     console.log(sessionStorage.getItem("pwdCorrect"));
 
-    let famArray = [["Familia Navratri {23}", "Per // 2023 // Navratri", "[DGMM]"], ["Familia Diwali {23}", "Per // Familia // Diwali", "[DGMM]"], ["Familia Random {23}", "Per // Familia // Radom", "[DGMM"], ["Dadi Bday", "Per // 2023 // Dadi Bday", "[DGMM]"], ["Onam + Nana & Mum + Others", "Alb // Fam", "[DMV5]"], ["Uk {23}", "Alb // Fam + Friends // Uk {23}", "[DMV5]"], ["Mum + Naina Aunty + Aaron + Vandana Aunty", "Alb // Fam + Friends", "[DMV5]"], ["Fui & Fua Anni", "Alb // Fui & Fua 25", "[DMV5]"], ["Nashik + Mumbai (Dada & I) {24}", "Alb // 2024 // Nashik + Bumbay", "[DMV5"]];
+    let famArray = [["Familia Navratri {23}", "Per // 2023 // Navratri", "[DGMM]"], ["Familia Diwali {23}", "Per // Familia // Diwali", "[DGMM]"], ["Familia Random {23}", "Per // Familia // Radom", "[DGMM"], ["Dadi Bday", "Per // 2023 // Dadi Bday", "[DGMM]"], ["Onam + Nana & Mum + Others", "Alb // Fam", "[DMV5]"], ["Uk {23}", "Alb // Fam + Friends // Uk {23}", "[DMV5]"], ["Mum + Naina Aunty + Aaron + Vandana Aunty", "Alb // Fam + Friends", "[DMV5]"], ["Fui & Fua Anni", "Alb // Fui & Fua 25", "[DMV5]"], ["Nashik + Mumbai (Dada & I) {24}", "Alb // 2024 // Nashik + Bumbay", "[DMV5]"], ["Ra Weds So {24}", "Ra Weds So", "[RWS1]"]];
 
     let perArray = [["College Dusshera", "Per // 2023 // clg-zindagi // dusshera", "[DGMM]"], ["Kpop + Gandhi Park", "Per // 2023 // Clg Zindagi // ....", "[DGMM]"], ["Clg + Everyone + class + ziya + otaku + deepz + anju + anu + nino + teachers...", "Per // 2023 // Clg Zindagi // Others", "[DGMM]"], ["The Final Days", "Per // 2023 // Clg Zindagi // Final Days", "[DGMM"], ["Airplanes", "Per // Airplanes", "[DGMM]"], ["16th Sep {23}", "Alb // Clg Zindagi // ....", "[DMV5]"], ["19th Jul | Fiza Nexus Jaga {23}", "Alb // Clg Zindagi // 19th Jul {23}", "[DMV5]"], ["Pratham Cam", "Alb // Clg Zindagi // Cam", "[DMV5]"], ["Onam + group + teachers + others {23}", "Alb // Clg Zindagi // Others", "[DMV5]"], ["Freshers {23}", "Alb // Clg Zindagi // ....", "[DMV5]"], ["Teachers", "Alb // Clg Zindagi // ....", "[DMV5]"], ["Talents Day + Panumbur Dariyo {23}", "Alb // Clg Zindagi // ....", "[DMV5]"], ["Traditional Day {23}", "Alb // Clg Zindagi // ....", "[DMV5]"], ["Nandu + Ramu + Lachu + Anu + Rosh + Amu + Others", "Alb // Sab Log", "[DMV5]"], ["Shazi + Amru + Nandhanas + Otaku/Darsh + Harsh + Raju", "Alb // Mine Hamesha // 2022", "[DMV5]"], ["School Onam + Movies + Farewell + Others", "Alb // Mine Hamesha // 2021 & Pehele", "[DMV5]"], ["Raju + Aaron + Amru + Suna", "Alb // Mine Hamesha // 2023", "[DMV5]"], ["Clg Onam {22} + Teachers", "Alb // Onam {22}", "[DMV5]"]];
 
@@ -43,7 +43,7 @@ const fillIt = () => {
 
 const fillBdays = () => {
   if (sessionStorage.getItem("pwdCorrect") == "yes" && sessionStorage.getItem("pwdChecked") == "yes") {
-    let bdayArray = [["Jan", 8, "Trisha"], ["Jan", 8, "Vaishnavi"], ["Jan", 16, "Didi"], ["Jan", 25, "Rajal"], ["Jan", 27, "Snehal"], ["Mar", 10, "Aishu (SIMS)"], ["Apr", 11, "Abhiram"], ["Apr", 19, "Brino"], ["Apr", 22, "Rachana"], ["Apr", 23, "Kumar"], ["May", 11, "Shravya"], ["May", 24, "Neha (SIMS)"], ["Jun", 12, "Raksha (GF)"], ["Jun", 16, "Darshan"], ["Jun", 20, "Vaishnavi (Goa)"], ["Jun", 23, "Deepz"], ["Jul", 1, "Ziya"], ["Jul", 14, "Anju"], ["Jul", 31, "Wizz"], ["Aug", 1, "Pratham"], ["Sep", 12, "Akash V"], ["Sep", 19, "Aaron"], ["Oct", 3, "Nivedh"], ["Nov", 3, "Otaku"], ["Nov", 5, "Nandu"], ["Nov", 14, "Anu Behen"], ["Nov", 20, "Sanjay (SIMS)"], ["Nov", 27, "Vinti"], ["Dec", 8, "Janani Miss"], ["Dec", 21, "Rohit (SIMS)"]];
+    let bdayArray = [["Jan", 8, "Trisha"], ["Jan", 8, "Vaishnavi"], ["Jan", 16, "Didi"], ["Jan", 25, "Rajal"], ["Jan", 27, "Snehal"], ["Feb", 4, "Mom & Dad Anniversary"], ["Feb", 22, "Dadi"], ["Mar", 10, "Aishu (SIMS)"], ["Apr", 11, "Abhiram"], ["Apr", 19, "Brino"], ["Apr", 22, "Rachana"], ["Apr", 23, "Kumar"], ["May", 11, "Shravya"], ["May", 16, "Mum"], ["May", 18, "Sauru"], ["May", 24, "Neha (SIMS)"], ["Jun", 12, "Raksha (GF)"], ["Jun", 16, "Darshan"], ["Jun", 20, "Vaishnavi (Goa)"], ["Jun", 23, "Deepz"], ["Jul", 1, "Ziya"], ["Jul", 14, "Anju"], ["Jul", 31, "Wizz"], ["Aug", 1, "Pratham"], ["Sep", 12, "Akash V"], ["Sep", 19, "Aaron"], ["Oct", 3, "Nivedh"], ["Nov", 3, "Otaku"], ["Nov", 5, "Nandu"], ["Nov", 14, "Anu Behen"], ["Nov", 20, "Sanjay (SIMS)"], ["Nov", 27, "Vinti"], ["Dec", 8, "Janani Miss"], ["Dec", 21, "Rohit (SIMS)"]];
 
     let printedArr = [];
 
@@ -74,13 +74,3 @@ const fillBdays = () => {
   }
 
 }
-
-let hamburgerMenu = document.getElementsByClassName("hamburger")[0];
-let theBar = Array.from(document.getElementsByClassName("bar"));
-
-hamburgerMenu.addEventListener("click", () => {
-  theBar.forEach(ele => {
-    ele.classList.toggle("active");
-  })
-  document.getElementsByClassName("second-nav")[0].classList.toggle("show");
-})
